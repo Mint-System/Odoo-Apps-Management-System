b3b63f5dee65084cf561026481353ba7db69dccc
Kurt Gisler <kurt.gisler@mint-system.ch>
2023-12-28 10:46:44 +0100

    Update

--- mgmt/models/mgmt_docustructure.py
+++ mgmt/models/mgmt_docustructure.py
@@ -4,9 +4,15 @@ class Management(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _name = "mgmt.docustructure"
     _description = "Docustructure"    
-    name = fields.Char("Title", required=True)
-    description = fields.Html("Description")
+    name = fields.Char("Title")
+    number = fields.Char("Number")
     parent_id = fields.Many2one("mgmt.docustructure",
-      string="Hierarchy")
+      string="Parent")
     document_id = fields.Many2one("mgmt.document",
-      string="Document")
+      string="Document", required=True)   
+    display_name = fields.Char(string="Display Name", compute="_compute_display_name", store=True)
+
+    @api.depends("number", "name")
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = f"{record.number} {record.name}"
--- mgmt/models/mgmt_paragraph.py
+++ mgmt/models/mgmt_paragraph.py
@@ -4,8 +4,17 @@ class Management(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _name = "mgmt.paragraph"
     _description = "Paragraph"    
-    name = fields.Char("Title", required=True)
-    description = fields.Html("Description")
+    name = fields.Char("Title")
+    description = fields.Text("Description")
     parent_id = fields.Many2one("mgmt.paragraph", "parent_id")
+    docustructure_id = fields.Many2one("mgmt.docustructure",
+      string="Docustructure", required=True)
     requirement_ids = fields.Many2many("mgmt.requirement", 
-      string="Requirement")    
+      string="Requirement")
+    display_name = fields.Char(string="Display Name", compute="_compute_display_name", store=True)
+
+    @api.depends("docustructure_id.number", "name")
+    def _compute_display_name(self):
+        for record in self:
+            paragraph_name = record.name if record.name else ""
+            record.display_name = f"{record.docustructure_id.number} {paragraph_name}"
