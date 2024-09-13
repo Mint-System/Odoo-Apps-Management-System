8528973a8623ffcaad8c6844a93bd2fd8b21361f
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-09-13 15:31:22 +0200

    feat: add fields

--- mgmt_asset/models/asset.py
+++ mgmt_asset/models/asset.py
@@ -10,3 +10,11 @@ class MgmtAsset(models.Model):
     _description = "Mgmt Asset"
 
     name = fields.Char()
+    model_name = fields.Selection([
+        ('product.product', 'Product'),
+        ('maintenance.equipment', 'Inventory'),
+        ('res.partner', 'Contact')
+    ])
+
+    model_id = fields.Many2one('ir.model')
+    res_id = fields.Integer(required=True)
--- mgmt_audit/models/audit.py
+++ mgmt_audit/models/audit.py
@@ -10,3 +10,13 @@ class MgmtAudit(models.Model):
     _description = "Mgmt Audit"
 
     name = fields.Char()
+    type = fields.Selection([  # Type of audit
+        ('internal', 'Internal'),
+        ('external', 'External'),
+        ('extreme', 'Extreme Audits')
+    ], required=True)
+    plan_end_date = fields.Date()  # Planned end date
+    stage_id = fields.Many2one('mgmt.audit.stage')  # Foreign key to audit stage
+    responsible_id = fields.Many2one('res.users')  # Responsible user for the audit
+    risk_ids = fields.Many2many('mgmt.risk')  # Many2many relationship with risks
+    statement_ids = fields.Many2many('mgmt.statement')
