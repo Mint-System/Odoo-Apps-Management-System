6e61af82818944cdbd416ed141ff79bfbaf95228
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-09-16 16:25:01 +0200

    feat: init mgmt_base module

---
+++ mgmt_base/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Mgmt Audit",
+    "summary": """
+        Audit your company.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "category": "Management",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mgmtsystem"],
+    "data": [
+        "views/menu.xml",
+   ],
+    "demo": ["demo/demo.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
