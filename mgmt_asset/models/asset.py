68e4cdf5caf8052f98706c2270eb654e51188037
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-09-17 16:28:24 +0200

    feat: refactor compute_name method

--- mgmt_asset/models/asset.py
+++ mgmt_asset/models/asset.py
@@ -4,12 +4,11 @@ from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
-
 class MgmtAsset(models.Model):
     _name = "mgmt.asset"
     _description = "Mgmt Asset"
 
-    name = fields.Char(compute='_compute_name', store=True)
+    name = fields.Char(compute='_compute_name', store=True, readonly=False)
     model_name = fields.Selection([
         ('res.partner', 'Contact'),
         ('product.product', 'Product'),
@@ -24,6 +23,11 @@ class MgmtAsset(models.Model):
     @api.depends('model_name', 'model_id', 'res_id')
     def _compute_name(self):
         for record in self:
-            if record.model_id:
-                linked_record = self.env[record.model_name].browse(record.res_id)
-                record.name = linked_record.display_name if linked_record else 'Unknown'
+            if record.model_name and record.res_id:
+                try:
+                    linked_record = self.env[record.model_name].browse(record.res_id)
+                    record.name = linked_record.display_name if linked_record else 'Unknown'
+                except KeyError:
+                    record.name = 'Unknown'
+            else:
+                record.name = 'Unknown'
