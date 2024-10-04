b43d9e4e848aaaee124746c9555efe3758072214
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-10-04 09:22:22 +0200

    feat: create revisions for hazards

--- mgmt_hazard_risk/models/hazard.py
+++ mgmt_hazard_risk/models/hazard.py
@@ -10,3 +10,17 @@ class MgmtHazard(models.Model):
 
     severity_id = fields.Many2one("mgmt.severity")
     probability_id = fields.Many2one("mgmt.probability")
+
+    def action_create_revision(self):
+        for hazard in self:
+            new_hazard = hazard.copy({
+                'name': f"{hazard.name} (Revision)",
+            })
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'New Hazard Revision',
+                'view_mode': 'form',
+                'res_model': 'mgmt.hazard',
+                'res_id': new_hazard.id,
+                'target': 'current',
+            }
