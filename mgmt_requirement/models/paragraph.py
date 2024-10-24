5fe3daf266a821c818f0b35bb99198889e2a9455
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-10-24 13:35:58 +0200

    feat: compute paragraph display_name

--- mgmt_requirement/models/paragraph.py
+++ mgmt_requirement/models/paragraph.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -14,3 +14,16 @@ class MgmtParagraph(models.Model):
     reference = fields.Char(required=True)
     document_id = fields.Many2one("mgmt.document")
     tag_ids = fields.Many2many("mgmt.requirement.tag")
+    display_name = fields.Char(compute="_compute_display_name", store=True)
+
+    @api.depends("name", "reference")
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = f"{record.name} ({record.reference})"
+
+    def name_get(self):
+        result = []
+        for paragraph in self:
+            name = f"{paragraph.name} ({paragraph.reference})"
+            result.append((paragraph.id, name))
+        return result
