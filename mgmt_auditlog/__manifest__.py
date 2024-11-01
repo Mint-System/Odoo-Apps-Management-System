507976e8075edb7ea76d59da6b035518c2b85f18
Janik von Rotz <login@janikvonrotz.ch>
2024-11-01 10:43:17 +0100

    feat: init mgmt_auditlog

---
+++ mgmt_auditlog/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Mgmt Auditlog",
+    "summary": """
+        Setup audit log rules for the mgmt modules.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/management-system",
+    "category": "Management",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mgmt_audit", "auditlog"],
+    "data": ["data/auditlog.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
