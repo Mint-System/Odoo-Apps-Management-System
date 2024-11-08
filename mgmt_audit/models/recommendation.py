84168a7b4b493b9650ad17f9cf349dbff276e5ea
Janik von Rotz <login@janikvonrotz.ch>
2024-11-08 09:00:38 +0100

    fix: show hazards

--- mgmt_audit/models/recommendation.py
+++ mgmt_audit/models/recommendation.py
@@ -8,6 +8,5 @@ _logger = logging.getLogger(__name__)
 class MgmtRecommendation(models.Model):
     _name = "mgmt.recommendation"
     _description = "Mgmt Recommendation"
-    _inherit = ["mail.thread", "mail.activity.mixin"]
 
     name = fields.Char(required=True)
