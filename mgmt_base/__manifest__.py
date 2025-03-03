08e974fd507d912fe8d1cb0954a5d1749bfd5829
Janik von Rotz <login@janikvonrotz.ch>
2025-03-03 11:15:15 +0100

    chore: website

--- mgmt_base/__manifest__.py
+++ mgmt_base/__manifest__.py
@@ -4,7 +4,7 @@
         Base for the management modules.
     """,
     "author": "Mint System GmbH",
-    "website": "https://github.com/OCA/sale-workflow",
+    "website": "https://www.mint-system.ch/",
     "category": "Management",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
