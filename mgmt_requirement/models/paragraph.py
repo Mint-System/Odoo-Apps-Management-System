a121b769f49c17e96d5592b3dd3f4ed1e265bf3a
Ulrich Kilian <uk@sdcience-and-more.de>
2026-01-29 13:26:46 +0100

    feat(mgmt_requirement): change display name of paragraph

--- mgmt_requirement/models/paragraph.py
+++ mgmt_requirement/models/paragraph.py
@@ -15,9 +15,23 @@ class MgmtParagraph(models.Model):
     document_id = fields.Many2one("mgmt.document")
     tag_ids = fields.Many2many("mgmt.requirement.tag")
 
-    def name_get(self):
-        result = []
-        for paragraph in self:
-            name = f"{paragraph.name} ({paragraph.reference})"
-            result.append((paragraph.id, name))
-        return result
+    display_name = fields.Char(
+        compute="_compute_display_name",
+        store=True,
+    )
+
+    @api.depends("name", "reference", "document_id.name")
+    def _compute_display_name(self):
+        for rec in self:
+            parts = []
+            if rec.document_id:
+                parts.append(f"{rec.document_id.name}")
+            if rec.reference:
+                parts.append(f"{rec.reference}")
+            if rec.name:
+                parts.append(rec.name)
+
+            if len(parts) > 1:
+                rec.display_name = " - ".join(parts)
+            else:
+                rec.display_name = "".join(parts)
