ba5f52e51179e148c65ac455295d709a143fcc16
Ulrich Kilian <uk@sdcience-and-more.de>
2026-02-11 17:34:32 +0100

    feat(mgmt_base): migrate

---
+++ mgmt_base/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Mgmt Base",
+    "summary": """
+        Base for the management modules.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch/",
+    "category": "Management",
+    "version": "19.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/system.xml",
+        "views/menu.xml",
+        "views/res_config_settings.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
