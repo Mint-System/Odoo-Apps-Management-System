8eea37dea986a7083c01919aba39ec2b2ca1d588
Ulrich Kilian <uk@sdcience-and-more.de>
2026-02-12 14:08:01 +0100

    fix(mgmt_asset): odoo version

--- mgmt_asset/__manifest__.py
+++ mgmt_asset/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch/",
     "category": "Management",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["mgmt_base"],
     "data": ["security/ir.model.access.csv", "views/asset_views.xml"],
