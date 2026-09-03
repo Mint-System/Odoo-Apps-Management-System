9c4a65720c65944767f1d2dcd12c708cc519f466
Janik von Rotz <login@janikvonrotz.ch>
2026-09-03 16:27:04 +0200

    chore(mgmt_audit): refactor inverse_stage_id logic and fix whitespace

--- mgmt_audit/models/audit.py
+++ mgmt_audit/models/audit.py
@@ -26,17 +26,16 @@ class MgmtAudit(models.Model):
         required=True,
         default=lambda self: self.env.ref("mgmt_audit.audit_stage_draft").id,
         group_expand="_read_group_stage_ids",
-        inverse = "_inverse_stage_id",
+        inverse="_inverse_stage_id",
     )
     responsible_id = fields.Many2one("hr.employee", tracking=True)
     statement_ids = fields.One2many("mgmt.statement", "audit_id")
     auditor_ids = fields.Many2many("res.partner")
 
     def _inverse_stage_id(self):
-        if self.stage_id.code == 'done':
-            for audit in self:
-                risk_ids = audit.statement_ids.risk_id + audit.statement_ids.requirement_id.risk_ids
-                risk_ids._compute_last_review_date()
+        for audit in self.filtered(lambda a: a.stage_id.code == "done"):
+            risk_ids = audit.statement_ids.risk_id + audit.statement_ids.requirement_id.risk_ids
+            risk_ids._compute_last_review_date()
 
     @api.model
     def _read_group_stage_ids(self, stages, domain):
