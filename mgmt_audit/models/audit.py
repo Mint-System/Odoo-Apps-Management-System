d4fd5bdfb20daf94b2f5fe2e9894c9cfbfbbd43c
laurens kooijman <laurenskooijman@gmail.com>
2026-09-03 16:08:47 +0200

    feat(mgmt_audit): inverse_date_planned removed, inverse_stage_id added

--- mgmt_audit/models/audit.py
+++ mgmt_audit/models/audit.py
@@ -19,25 +19,24 @@ class MgmtAudit(models.Model):
         ],
         tracking=True,
     )
-    planned_date = fields.Date(tracking=True, inverse="_inverse_planned_date")
+    planned_date = fields.Date(tracking=True)
     stage_id = fields.Many2one(
         "mgmt.audit.stage",
         tracking=True,
         required=True,
         default=lambda self: self.env.ref("mgmt_audit.audit_stage_draft").id,
         group_expand="_read_group_stage_ids",
+        inverse = "_inverse_stage_id",
     )
     responsible_id = fields.Many2one("hr.employee", tracking=True)
     statement_ids = fields.One2many("mgmt.statement", "audit_id")
     auditor_ids = fields.Many2many("res.partner")
 
-    def _inverse_planned_date(self):
-        """
-        Connection to risk is: audit_id -> statement_ids -> risk_id
-        """
-        for audit in self:
-            risk_ids = audit.statement_ids.risk_id + audit.statement_ids.requirement_id.risk_ids
-            risk_ids._compute_last_review_date()
+    def _inverse_stage_id(self):
+        if self.stage_id.code == 'done':
+            for audit in self:
+                risk_ids = audit.statement_ids.risk_id + audit.statement_ids.requirement_id.risk_ids
+                risk_ids._compute_last_review_date()
 
     @api.model
     def _read_group_stage_ids(self, stages, domain):
